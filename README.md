9f290d96492b0cff2b64218705e9bf117e1929c6
David Regan <daveregan@gmail.com>
2023-08-11 13:31:15 +0100

    adding reset function

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ The big button what records the trucks!
 
 Requires the DigiKeyboard.h library, which is included in the Arduino IDE's DigiSpark package.
 
-## Simple Usage Instructions
+## Setup Instructions
 In Arduino IDE, go to Tools -> Boards -> Board Manager and install DigiStump AVR Boards.
 
 Once instealled, Under Tools -> Boards -> DigiStump AVR Boards, select your board (I went with Default and it worked just fine).
@@ -15,6 +15,13 @@ Connect that pin to the button and the other side to GND.
 Run code.
 
 You can optionally hook an LED across pin 1 and GND and it will illuminate at the same time as the board LED, which is fun and cool.
+
+## Usage
+Do something cool in a videogame and then smash that like button
+
+The button will be disabled for five seconds (denoted by the LED illuminating) between records, just to prevent bouncing trying to trigger multiple records and crashing your computer.
+
+You can reset the board by holding the button down for three seconds, let go when it flashes and it will restart.
 
 ## Further development
 Probably make it so the pin isn't an INPUT_PULLUP, I dunno
--- dave-o-rec.ino
+++ dave-o-rec.ino
@@ -4,12 +4,19 @@ const int pinLED = 1;
 const int pinBTN = 2;
 
 int btnState = 0;
-int lastbtnState = 0;
+int lastbtnState = HIGH;
+
+bool gameRecording = false;
+int btnHeld = 0;
+const int btnHeldMax = 3000;
+
+void (* resetDigispark) (void) = 0;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(pinLED, OUTPUT); //onboard LED
   pinMode(pinBTN, INPUT_PULLUP); //button pin w/ pullup inline
+  lastbtnState = digitalRead(pinBTN);
 }
 
 void loop() {
@@ -17,12 +24,26 @@ void loop() {
   DigiKeyboard.update();
   btnState = digitalRead(pinBTN);
   if (btnState == LOW && btnState != lastbtnState) {
-    digitalWrite(pinLED, HIGH);
-    recGame();
+    btnHeld = 0;
+    delay(10);
+  }
+  
+  else if (btnState == LOW && btnState == lastbtnState) {
+    btnHeld = btnHeld + 100;
+    if (btnHeld > btnHeldMax) {
+      resetBoard();
+    } 
+    delay(100);
   }
 
   else if (btnState == HIGH && btnState != lastbtnState) {
-    digitalWrite(pinLED, LOW);
+    digitalWrite(pinLED, HIGH);
+    recGame();
+  }
+  
+  else if (btnState == HIGH && btnState == lastbtnState && gameRecording == true) {
+    digitalWrite(pinLED, LOW);  
+    gameRecording == false;
   }
 
   lastbtnState = btnState;
@@ -31,6 +52,19 @@ void loop() {
 
 void recGame() {
   DigiKeyboard.sendKeyStroke(KEY_G, MOD_ALT_LEFT | MOD_GUI_LEFT);
+  gameRecording = true;
   DigiKeyboard.delay(5000);
   return;
+}
+
+void resetBoard() {
+  for (int i = 0; i <= 5; i++) {
+    digitalWrite(pinLED, HIGH);
+    delay(100);
+    digitalWrite(pinLED, LOW);
+    delay(100);
+  }
+
+  resetDigispark();
+    
 }
